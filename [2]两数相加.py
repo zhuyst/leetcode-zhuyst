cd6fc3accdf721e1c37cb28856ee0612ee426659
zhuyst <13553371651@139.com>
2019-05-08 17:16:33 +0800

    feat([2]两数相加): AC

---
+++ [2]两数相加.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+# 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+#
+# 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+#
+# 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+#
+# 示例：
+#
+# 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+# 输出：7 -> 0 -> 8
+# 原因：342 + 465 = 807
+#
+#
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        is_up = False
+        rs = ListNode(0)
+        now = rs
+
+        while l1 or l2:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+            if l2:
+                v2 = l2.val
+            val = v1 + v2
+
+            if is_up:
+                is_up = False
+                val = val + 1
+
+            if val >= 10:
+                is_up = True
+                val = val % 10
+
+            now.val = val
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+            if not l1 and not l2 and is_up:
+                new = ListNode(1)
+                now.next = new
+                now = new
+            elif l1 or l2:
+                new = ListNode(0)
+                now.next = new
+                now = new
+
+        return rs
